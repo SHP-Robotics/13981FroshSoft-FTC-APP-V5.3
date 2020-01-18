6dca842cea2aa2511bf718ac78fbc9c326eddc15
alexapico <bcraftyminer@gmail.com>
2020-01-18 15:04:56 -0800

    Uses Servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftLoadingZoneAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftLoadingZoneAutonomous.java
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 public class LeftLoadingZoneAutonomous extends BaseRobot {
     private int stage;
-
     @Override
     public void init() {
         super.init();
@@ -23,41 +22,40 @@ public class LeftLoadingZoneAutonomous extends BaseRobot {
     public void loop() {
         super.loop();
         switch (stage) {
-
+            //center with Loading Zone
             case 0:
-                    if (auto_mecanum(0.3,7)){
-                        reset_drive_encoders();
-                        stage++;
-
-                    }
-            break;
+                if (auto_mecanum(0.4,7)){
+                    reset_drive_encoders();
+                    stage++;
+                }
+                break;
+                //Move forward and grab
             case 1 :
-                    if (auto_drive(1, 33)) {
-                        set_platformMoveLeft_servo(0);
-                        set_platformMoveRight_servo(1);
-                        reset_drive_encoders();
-                        timer.reset();
-                        stage++;
-                    }
+                if (auto_drive(0.6, 33)) {
+                    set_platformMoveLeft_servo(0);
+                    set_platformMoveRight_servo(1);
+                    reset_drive_encoders();
+                    timer.reset();
+                    stage++;
+                }
 
                 break;
-
+            //Reverses and then releases platform
             case 2:
                 if (timer.seconds() > 1) {
-                    if (auto_drive(-1, 31)) {
-                        set_platformMoveLeft_servo(1);
-                        set_platformMoveRight_servo(0);
+                    if (auto_drive(-0.6, 31)) {
+                        set_platformMoveLeft_servo(1.8);
+                        set_platformMoveRight_servo(0.2);
                         reset_drive_encoders();
                         timer.reset();
                         stage++;
                     }
                 }
                 break;
-
+            //mecanums to parking after 2 seconds
             case 3:
-                if (timer.seconds() > 1) {
+                if (timer.seconds() > 2) {
                     if (auto_mecanum(-0.5, 60)) {
-                        setArmClampMotor(0);
                         reset_drive_encoders();
                         stage++;
                     }
